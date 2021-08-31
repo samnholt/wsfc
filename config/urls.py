f886fc2e37cee189200e8492f978fc5724ae71e7
samnholt <samnholt@hotmail.co.uk>
2021-08-31 18:48:54 +0100

    security

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Django admin
+    path('stater/', admin.site.urls),
 
     # Local apps
     path('', include('pages.urls')),
